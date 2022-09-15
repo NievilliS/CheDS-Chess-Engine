b4e78b142eacb905e62318ae76127350972b7675
NievilliS <d.g.schoosi@gmail.com>
2022-09-15 10:30:13 +0200

    winapi example

--- winplg.c
+++ winplg.c
@@ -14,6 +14,7 @@
 //#ifdef PRODUCTION_MODE
 
 /***** INCLUDES *****/
+#include <stdio.h>
 #include <windows.h>
 #include "winresources.h"
 
@@ -35,28 +36,67 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
             switch(LOBYTE(wParam)) {
                 case IDRC_NEW:
                     MessageBox(NULL, "New Button pressed!", "Menubutton",
-                        MB_ICONHAND | MB_OK);
+                        MB_ICONEXCLAMATION | MB_OK);
                 break;
                 case IDRC_OPEN:
                     MessageBox(NULL, "Open Button pressed!", "Menubutton",
-                        MB_ICONHAND | MB_OK);
+                        MB_ICONEXCLAMATION | MB_OK);
                 break;
                 case IDRC_SAVE:
                     MessageBox(NULL, "Save Button pressed!", "Menubutton",
-                        MB_ICONHAND | MB_OK);
+                        MB_ICONEXCLAMATION | MB_OK);
                 break;
                 case IDRC_SAVEAS:
                     MessageBox(NULL, "Save As Button pressed!", "Menubutton",
-                        MB_ICONHAND | MB_OK);
+                        MB_OK);
                 break;
                 case IDRC_EXIT:
-                    SendMessage(hwnd, WM_CLOSE, NULL, NULL);
+                    SendMessage(hwnd, WM_CLOSE, (WPARAM) NULL, (LPARAM) NULL);
                 break;
             }
         break;
         case WM_PAINT:
         {
+            // Just a note, never use a MessageBox from inside WM_PAINT
+			// The box will cause more WM_PAINT messages and you'll probably end up
+			// stuck in a loop
 
+			BITMAP bm;
+			PAINTSTRUCT ps;
+            LPRECT rect;
+            GetWindowRect(hwnd, rect);
+
+            int _width = (rect->right - rect->left);
+            int _height = (rect->bottom - rect->top);
+            int _size = _width * _height;
+
+            COLORREF _pixels[_size];
+            for(int y = 0; y < _height; y++) for(int x = 0; x < _width; x++) {
+                if((!y) || (!x) || (y == _height - 1) || (x == _width - 1))
+                    _pixels[x + y * _width] = 0xFF000000;
+                else
+                    _pixels[x + y * _width] = 0xFFAAFFFF;
+            }
+
+            HBITMAP g_hbmTest = CreateBitmap(
+                (rect->right - rect->left) - 10,
+                (rect->bottom - rect->top) - 10,
+                32, 1, _pixels
+            );
+			HDC hdc = BeginPaint(hwnd, &ps);
+
+			HDC hdcMem = CreateCompatibleDC(hdc);
+			HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMem, g_hbmTest);
+
+			GetObject(g_hbmTest, sizeof(bm), &bm);
+
+			BitBlt(hdc, 5, 5, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, SRCCOPY);
+
+			SelectObject(hdcMem, hbmOld);
+			DeleteDC(hdcMem);
+            DeleteObject(g_hbmTest);
+
+			EndPaint(hwnd, &ps);
         }
         break;
         default:
